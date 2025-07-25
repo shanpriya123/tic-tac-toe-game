5bdd1184f5f4094ca440ffe4d2817983bddbedd8
shanpriya123 <shanmu45gp@gmail.com>
2025-07-25 08:31:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-
+const players = { 
+  player1: { type: 'human', symbol: 'X'},
+  player2: { type: 'ai', symbol: 'O'}
+};
+let CurrentPlayer = 'player1';
+const board = Array(a).fill(null);
+function playTurn(index) {
+  if (board[index] === null) {
+    board[index] = players[CurrentPlayer].symbol;
+    CurrentPlayer = (CurrentPlayer === 'player1')? 'player2':'player1';
+    if (players[currentplayer].type === 'ai') {
+      const aiMove = getBestMove(board,players[CurrentPlayer].symbol);
+      playTurn(aiMove);
+    }
+  }
+}
