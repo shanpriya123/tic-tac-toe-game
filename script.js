8bcd3752b5c0f20d662426d51a01c3648605e58a
shanpriya123 <shanmu45gp@gmail.com>
2025-08-11 08:28:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-if (checkWinner()) {
-  document.getElementById("result").innerText = currentPlayer + " Wins!";
-} else if (moves === 9) {
-  document.getElementById("result").innerText = "It's a Draw!";
-}
+
